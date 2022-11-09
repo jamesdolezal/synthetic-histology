9e964a3161ae1a80f01969355f7d6531889ba5b7
James <jamesmdolezal@gmail.com>
2022-11-09 16:30:22 -0600

    Tensorflow allow GPU memory growth

--- concordance.py
+++ concordance.py
@@ -9,6 +9,12 @@ import slideflow as sf
 from typing import List
 from slideflow.gan.interpolate import StyleGAN2Interpolator
 
+# Allow GPU memory growth, so Tensorflow & PyTorch can play nice
+import tensorflow as tf
+gpus = tf.config.experimental.list_physical_devices('GPU')
+for gpu in gpus:
+    tf.config.experimental.set_memory_growth(gpu, True)
+
 # -----------------------------------------------------------------------------
 
 def num_range(s: str) -> List[int]:
--- interpolation_probability.py
+++ interpolation_probability.py
@@ -14,6 +14,12 @@ from os.path import join
 from typing import List
 from slideflow.gan.interpolate import StyleGAN2Interpolator
 
+# Allow GPU memory growth, so Tensorflow & PyTorch can play nice
+import tensorflow as tf
+gpus = tf.config.experimental.list_physical_devices('GPU')
+for gpu in gpus:
+    tf.config.experimental.set_memory_growth(gpu, True)
+
 # -----------------------------------------------------------------------------
 
 def num_range(s: str) -> List[int]:
